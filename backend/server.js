605b75eba70b86b20ab720d2b31fb21af0db1f1e
gashaw mucheye <gashaw2533@gmail.com>
2025-10-13 19:48:48 +0300

    Implement messaging functionality with routes for contacts, chat partners, and message handling; add Cloudinary configuration for image uploads

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import colors from 'colors';
 import connectDb from './lib/db.js';
 import { ENV } from './lib/env.js';
 import authRoute from './routes/authRoutes.js';
+import messageRoute from './routes/messageRoute.js';
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
@@ -12,6 +13,7 @@ app.use(cookieParser());
 const PORT = ENV.PORT || 8080;
 app.use(express.json());
 app.use('/auth', authRoute);
+app.use('/messages', messageRoute);
 app.listen(PORT, () => {
   console.log(
     `Server running on port: http://localhost:${PORT}`.bgYellow.underline
