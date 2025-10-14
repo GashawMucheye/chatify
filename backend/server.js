fd9a07d0fcc7a4d78d5a1f6c57b5e168ceeaeb95
gashaw mucheye <gashaw2533@gmail.com>
2025-10-14 16:22:17 +0300

    Implement socket.io for real-time communication; add socket authentication middleware and update server initialization

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,16 @@ import connectDb from './lib/db.js';
 import { ENV } from './lib/env.js';
 import authRoute from './routes/authRoutes.js';
 import messageRoute from './routes/messageRoute.js';
-const app = express();
+import { app, server } from './lib/socket.js';
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 const PORT = ENV.PORT || 8080;
-app.use(express.json());
+
 app.use('/auth', authRoute);
 app.use('/messages', messageRoute);
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(
     `Server running on port: http://localhost:${PORT}`.bgYellow.underline
   );
